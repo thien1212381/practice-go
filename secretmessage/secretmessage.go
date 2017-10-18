af9b6e8894f3b4579ae28072d9dc79d79167f6a5
thien1212381 <mrthien30@gmail.com>
2017-10-18 18:05:08 +0700

    Implemented Secret Message

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -1,6 +1,41 @@
 package secretmessage
 
+import "sort"
+
 // Encode func
 func Encode(encoded string) string {
-	return ""
+	countData := map[int32]int{}
+
+	for _, c := range encoded {
+		if count,ok := countData[c]; ok {
+			countData[c] = count + 1
+		} else {
+			countData[c] = 0
+		}
+	}
+
+	mapList := map[int][]int32{}
+	countArr := []int{}
+
+	for k,v := range countData {
+		mapList[v] = append(mapList[v], k)
+	}
+
+	for count := range mapList {
+		countArr = append(countArr, count)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(countArr)))
+
+	var result string
+	for _, count := range countArr {
+		for _, c := range mapList[count] {
+			if string(c) == "_" {
+				return result
+			}
+			result += string(c)
+		}
+	}
+	return result
+
 }
